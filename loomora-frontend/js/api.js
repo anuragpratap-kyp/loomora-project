8a34b38c0875642631fcd93dc7236cd70c9d5afb
Anurag  kashyap <anurag82006@gmail.com>
2025-08-16 22:45:24 +0530

    Update api.js

--- loomora-frontend/js/api.js
+++ loomora-frontend/js/api.js
@@ -1,5 +1,6 @@
 // --- API Base ---
-const API_BASE = localStorage.getItem('API_BASE') || 'http://localhost:5000/api';
+const API_BASE = localStorage.getItem('API_BASE') || "https://loomora-weav.onrender.com";
+';
 
 // --- Helpers ---
 const token = () => localStorage.getItem('token');
